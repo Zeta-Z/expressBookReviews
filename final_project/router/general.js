d8184781210367091b9fd9d10affb89643df558c
Zeta-Z <suescumperea@gmail.com>
2025-07-17 12:11:12 -0500

    Commit

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,6 +4,7 @@ let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
+const axios = require("axios");
 
 public_users.post("/register", (req, res) => {
   //Write your code here
@@ -30,8 +31,25 @@ public_users.get("/", function (req, res) {
 
   const libros = JSON.stringify(books);
 
-  return res.status(300).json({ message: "Books found" + libros });
+  return res.status(200).json({ message: "Books found" + libros });
 });
+
+//Using AXIOS to get all books!
+
+public_users.get("/axios", function(req,res){
+  axios.get("http://localhost:5000/")
+
+  .then(response =>{
+    return res.status(200).json(response.data);
+  })
+
+  .catch(error =>{
+    return res.status(500).json({message: "Error!", error: error.message})
+  })
+
+})
+
+
 
 // Get book details based on ISBN
 public_users.get("/isbn/:isbn", function (req, res) {
@@ -42,11 +60,31 @@ public_users.get("/isbn/:isbn", function (req, res) {
   if (id) {
     const libroEncontrado = JSON.stringify(books[id]);
 
-    return res.status(300).json({ message: "Book found" + libroEncontrado });
+    return res.status(200).json({ message: "Book found" + libroEncontrado });
   }else{
     return res.status(400).json({message: "Error, book not found"})
   }
 });
+
+
+//Get book details based on ISBN with AXIOS!
+
+public_users.get("/axios/isbn/:isbn", function(req,res){
+
+  const isbn = req.params.isbn;
+
+  axios.get(`http://localhost:5000/isbn/${isbn}`)
+
+  .then(response =>{
+    return res.status(200).json(response.data);
+  })
+
+  .catch(error =>{
+    return res.status(500).json({message: "Error!", error: error.message})
+  })
+
+
+})
 
 // Get book details based on author
 public_users.get("/author/:author", function (req, res) {
@@ -62,12 +100,33 @@ public_users.get("/author/:author", function (req, res) {
       }
     }
 
-    return res.status(300).json({ message: `Lista de Libros basada en ${author}`+ JSON.stringify(arregloAutores) });
+    return res.status(200).json({ message: `Lista de Libros basada en ${author}`+ JSON.stringify(arregloAutores) });
   }else{
     return res.status(400).json({message:" Error, author no valido"})
   }
   
 });
+
+//Get book by author with AXIOS
+public_users.get("/axios/author/:author", function(req,res){
+
+  const author = req.params.author;
+
+  axios.get(`http://localhost:5000/author/${author}`)
+
+  .then(response =>{
+    return res.status(200).json(response.data);
+  })
+
+  .catch(error =>{
+    return res.status(500).json({message: "Error!", error: error.message})
+  })
+
+
+})
+
+
+
 
 // Get all books based on title
 public_users.get("/title/:title", function (req, res) {
@@ -90,9 +149,24 @@ public_users.get("/title/:title", function (req, res) {
   }else{
     return res.status(400).json({message:"Invalid title"});
   }
-
-
 });
+
+public_users.get("/axios/title/:title", function(req,res){
+
+  const title = req.params.title;
+
+  axios.get(`http://localhost:5000/title/${title}`)
+
+  .then(response =>{
+    return res.status(200).json(response.data);
+  })
+
+  .catch(error =>{
+    return res.status(500).json({message: "Error!", error: error.message})
+  })
+
+
+})
 
 //  Get book review
 public_users.get("/review/:isbn", function (req, res) {
